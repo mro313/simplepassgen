65ec21f767f8d22af4974d68578991ac0732bb8e
mro313 <36976869+mro313@users.noreply.github.com>
2021-02-23 16:58:10 -0500

    initial update
    added .html and .js file

---
+++ simplepassgen.js
@@ -0,0 +1,11 @@
+
+
+function generate() {
+    document.getElementById("demo").innerHTML = "sdkjfb24792#8sdfku@!";
+}
+
+
+function othername() {
+    var input = document.getElementById("userInput").value;
+    document.getElementById("testtest").innerHTML = input;
+}
