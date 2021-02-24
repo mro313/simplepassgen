27e2cc85a2c502b72e12e69b9082e265741a2ef9
mro313 <36976869+mro313@users.noreply.github.com>
2021-02-24 18:08:42 -0500

    more updates

--- simplepassgen.js
+++ simplepassgen.js
@@ -5,7 +5,26 @@ function generate() {
 }
 
 
-function othername() {
-    var input = document.getElementById("userInput").value;
-    document.getElementById("testtest").innerHTML = input;
+function generatePassword() {
+
+    // 1)Get all the fields value
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var favoriteFood = document.getElementById("favoriteFood").value;
+    var favoriteColor = document.getElementById("favoriteColor").value;
+    var number = document.getElementById("number").value;
+    // 2) write a function to pass those values and get  value
+    var result   = buildRandomPassword(firstName, lastName, favoriteFood, favoriteColor, number)
+
+    // 3) assgn and display
+
+    document.getElementById("randompassword").innerHTML = result;
+    
+    
+}
+
+function buildRandomPassword(firstName, lastName, favoriteFood, favoriteColor, number) {
+
+    return (firstName + lastName + favoriteFood + favoriteColor + number);
+
 }
